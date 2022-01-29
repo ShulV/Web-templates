f1432322648e623439f25f498d029f73875552e3
Victor Desctop <vshulpov@gmail.com>
2022-01-29 10:51:24 +0700

    scroll

---
+++ scroll-through-anchors/script.js
@@ -0,0 +1,51 @@
+document.querySelectorAll('.js-scroll-link').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const href = this.getAttribute('href').substring(1);
+    const scrollTarget = document.getElementById(href);
+    const elementPosition = scrollTarget.getBoundingClientRect().top;
+
+    window.scrollBy({
+      top: elementPosition,
+      behavior: 'smooth'
+    });
+  });
+});
+// СКРОЛЛ ТОЛЬКО НА МОБИЛЬНОМ
+
+// const MOBILE_WIDTH = 580;
+
+// function getWindowWidth () {
+//   return Math.max(
+//     document.body.scrollWidth,
+//     document.documentElement.scrollWidth,
+//     document.body.offsetWidth,
+//     document.documentElement.offsetWidth,
+//     document.body.clientWidth,
+//     document.documentElement.clientWidth
+//   );
+// }
+
+// function scrollToContent (link, isMobile) {
+// 	if (isMobile && getWindowWidth() > MOBILE_WIDTH) {
+// 		return;
+// 	}
+
+//   const href = link.getAttribute('href').substring(1);
+//   const scrollTarget = document.getElementById(href);
+//   const elementPosition = scrollTarget.getBoundingClientRect().top;
+
+//   window.scrollBy({
+//       top: elementPosition,
+//       behavior: 'smooth'
+//   });
+// }
+
+// document.querySelectorAll('.js-scroll-link').forEach(link => {
+//   link.addEventListener('click', function(e) {
+//       e.preventDefault();
+
+//       scrollToContent(this, true);
+//   });
+// });
